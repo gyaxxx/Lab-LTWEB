089254e3ec15471840d7b68e57866b11afbb2449
gyaxx <gyax241@gmail.com>
2023-09-13 22:22:04 +0700

    first commit

---
+++ Thuc_Hanh_1/Models/Branch.cs
@@ -0,0 +1,7 @@
+﻿namespace Thuc_Hanh_1.Models
+{
+    public enum Branch
+    {
+        IT, BE, CE, EE
+    }
+}
---
+++ Thuc_Hanh_1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Thuc_Hanh_1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
